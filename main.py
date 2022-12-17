f47c1120e0db30319641cbc816200f16de41886a
Gabriele <gabryconte90@gmail.com>
2022-12-17 18:05:14 +0100

    Modified version with online API's

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from question_model import Question
 from data import question_data
-from quiz_brain import *
-from time import *
+from quiz_brain import QuizBrain
+from ui import QuizInterface
+
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(text=question_text, answer=question_answer)
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-quiz = QuizBrain(question_bank)
-while quiz.question_number != len(quiz.questions_list):
-    quiz.nextquestion()
 
-print(f"Congratulations! You managed to guess {quiz.points} answers out of {len(quiz.questions_list)} questions!")
-sleep(10)
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,20 +1,28 @@
+import html
+
+
 class QuizBrain:
-    def __init__(self, qlist):
+
+    def __init__(self, q_list):
         self.question_number = 0
-        self.questions_list = qlist
-        self.points = 0
+        self.score = 0
+        self.question_list = q_list
+        self.current_question = None
 
-    def nextquestion(self):
-        question = self.questions_list[self.question_number]
-        k = input(f"Q.{self.question_number + 1}: {question.text}. (True\False)?")
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        self.checkquestion(k,question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
 
-    def checkquestion(self, k,question):
-        if question == k.capitalize():
-            self.points += 1
-            print("\n"+rf"Right answer! Your right guesses are {self.points}\{self.question_number} ")
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            return True
         else:
-            print("\n"+rf"Wrong answer! Your right guesses are {self.points}\{self.question_number} ")
-        print(f"The correct answer was {question}")
-        
+            return False
+
